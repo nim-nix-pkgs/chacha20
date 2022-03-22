243546a7dc8e0c03809e0d66e7532fd60b070123
hugosenari <hugosenari@gmail.com>
2022-03-22 01:07:51 -0300

    chore: re-index ../nimpkgs/c/chacha20

--- trunk/flake.nix
+++ trunk/flake.nix
@@ -8,8 +8,8 @@
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
   inputs.src-chacha20-trunk.flake = false;
+  inputs.src-chacha20-trunk.ref   = "refs/heads/trunk";
   inputs.src-chacha20-trunk.owner = "~ehmry";
-  inputs.src-chacha20-trunk.ref   = "trunk";
   inputs.src-chacha20-trunk.repo  = "chacha20";
   inputs.src-chacha20-trunk.type  = "sourcehut";
   
